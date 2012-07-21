6f350f491a04713d64ac96c06512997aab249249
Kipp Hickman <kipp@Kipps-MacBook-Pro.local>
2012-07-21 16:11:55 -0700

    Added my-python-clear-buffer, and bound C-c C-k to it when in python-mode.

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
  '(c-tab-always-indent nil)
  '(c-toggle-auto-hungry-state nil)
  '(column-number-mode t)
+ '(comint-process-echoes t)
  '(custom-safe-themes (quote ("501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" default)))
  '(haskell-mode-hook (quote (turn-on-haskell-indent turn-on-haskell-doc-mode)))
  '(ido-ignore-files (quote ("\\`CVS/" "\\`#" "\\`.#" "\\`\\.\\./" "\\`\\./" "\\.DS_Store")))
@@ -27,7 +28,6 @@
  '(ns-command-modifier (quote meta))
  '(ns-right-alternate-modifier (quote super))
  '(python-guess-indent nil)
- '(quack-fontify-style nil)
  '(scroll-bar-mode nil)
  '(standard-indent 2)
  '(tool-bar-mode nil))
@@ -38,12 +38,29 @@
 
 ;; Setup python stuff
 (require 'virtualenv)
-;; set C-c C-v to use my virtualenv
+
+;; (defun my-python-send-buffer ()
+;;   "Clear the python buffer, then send the buffer"
+;;   (interactive)
+;;   (when python-buffer
+;;     (with-current-buffer python-buffer
+;;       (erase-buffer)
+;;       (python-send-string "\n")))
+;;   (python-send-region-and-go (point-min) (point-max)))
+
+(defun my-python-clear-buffer ()
+  "Clear the python buffer, if it is running"
+  (interactive)
+  (when python-buffer
+    (with-current-buffer python-buffer
+      (erase-buffer)
+      (python-send-string "\n"))))
+    
+;; Modify some keys in python mode
 (add-hook 'python-mode-hook
           '(lambda ()
-             (define-key python-mode-map
-               (kdb "C-c C-v")
-               'virtualenv-run-python)))
+             (define-key python-mode-map "\C-c\C-v" 'virtualenv-run-python)
+             (define-key python-mode-map "\C-c\C-k" 'my-python-clear-buffer)))
 
 
 ;; put the autosave and backup files in my emacs directory
@@ -97,3 +114,8 @@
 (setq ido-everywhere t)
 (setq ido-create-new-buffer 'always)
 (setq confirm-nonexistent-file-or-buffer nil)
+
+;; Run Emacs as a server
+(require 'server)
+(unless (server-running-p)
+  (server-start))
