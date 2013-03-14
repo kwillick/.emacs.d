9e570e1b567b36c9bcf56bdf4725df50ea31204a
Kipp Hickman <kipp.hickman@oomnitza.com>
2013-03-14 14:49:10 -0700

    Removed separate setup-appearance file. Removed melpa package archive and replaced with marmalade.

--- init.el
+++ init.el
@@ -1,59 +1,39 @@
-;;
-;; init.el - My .emacs files
-;;
-;; Copyright (c) 2012, Kipp Hickman
-;; All rights reserved.
-;;
-;; Redistribution and use in source and binary forms, with or without
-;; modification, are permitted provided that the following conditions are met:
-;;  * Redistributions of source code must retain the above copyright
-;;    notice, this list of conditions and the following disclaimer.
-;;  * Redistributions in binary form must reproduce the above copyright
-;;    notice, this list of conditions and the following disclaimer in the
-;;    documentation and/or other materials provided with the distribution.
-;;
-;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-;; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-;; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-;; A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-;; OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-;; SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-;; TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-;; PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-;; LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+(require 'cl-lib)
 
+;; Setup Emacs PATH
+(defun my-set-path-from-shell ()
+  (let ((path-from-shell
+         (shell-command-to-string "$SHELL -l -c 'echo -n $PATH'")))
+  (setenv "PATH" path-from-shell)
+  (setq exec-path (split-string path-from-shell path-separator))))
+  
+(my-set-path-from-shell)
 
-(add-to-list 'exec-path "/usr/local/bin/")
-
+;; Setup load-path
 (add-to-list 'load-path (expand-file-name user-emacs-directory))
 
-
-(require 'cl)
-
-;; Add melpa package archive
+;; Add marmalade package archive
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives 
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
 
 (defvar my-packages
-  '(magit haskell-mode solarized-theme yasnippet)
+  '(magit haskell-mode solarized-theme yasnippet markdown-mode expand-region)
   "A list of the packages I want to ensure are installed")
 
 ;; Based off of preludes prelude-install-packages
 (defun my-ensure-packages-installed ()
-  (unless (every 'package-installed-p my-packages))
+  (unless (cl-every 'package-installed-p my-packages)
     (message "%s" "Some packages are missing. Refreshing package database...")
     (package-refresh-contents)
     (message "%s" "Done refreshing package database")
     ;;install them
     (dolist (pack my-packages)
       (unless (package-installed-p pack)
-        (package-install pack))))
+        (package-install pack)))))
   
 (my-ensure-packages-installed)
 
@@ -66,7 +46,7 @@
  '(c-basic-offset 4)
  '(c-default-style (quote ((c-mode . "bsd") (c++-mode . "bsd") (java-mode . "bsd") (awk-mode . "awk") (other . "bsd"))))
  '(c-echo-syntactic-information-p t)
- '(c-electric-pound-behavior nil)
+ '(c-electric-pound-behavior (quote (alignleft)))
  '(c-hanging-braces-alist (quote ((defun-close) (class-close) (inline-close) (block-close) (statement-cont) (substatement-open after) (brace-list-open) (brace-list-close) (brace-entry-open) (extern-lang-open after) (namespace-open after) (namespace-close) (module-open after) (module-close) (composition-open after) (composition-close) (inexpr-class-open after) (inexpr-class-close before) (arglist-cont-nonempty))))
  '(c-offsets-alist (quote ((access-label . /) (arglist-close . 0) (inextern-lang . 0) (innamespace . 0))))
  '(c-tab-always-indent nil)
@@ -74,6 +54,7 @@
  '(column-number-mode t)
  '(comint-process-echoes t)
  '(custom-safe-themes (quote ("501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" default)))
+ '(default-frame-alist (quote ((vertical-scroll-bars) (width . 100) (height . 66))))
  '(haskell-mode-hook (quote (turn-on-haskell-indent turn-on-haskell-doc-mode)))
  '(ido-ignore-files (quote ("\\`CVS/" "\\`#" "\\`.#" "\\`\\.\\./" "\\`\\./" "\\.DS_Store")))
  '(indent-tabs-mode nil)
@@ -82,17 +63,39 @@
  '(ns-command-modifier (quote meta))
  '(ns-right-alternate-modifier (quote super))
  '(python-guess-indent nil)
+ '(python-indent-guess-indent-offset nil)
  '(scroll-bar-mode nil)
  '(standard-indent 2)
+ '(tab-width 4)
  '(tool-bar-mode nil))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "apple" :family "Monaco")))))
 
-(require 'setup-appearance)
+
+;; Load theme
+(require 'solarized-theme)
+(load-theme 'solarized-dark t)
+
+
+;; Format title to show file path or buffer name
+(defun my-frame-title-format ()
+  (let ((buf-name (buffer-file-name)))
+    (if buf-name
+        (abbreviate-file-name buf-name)
+      "%b")))
+
+(setq frame-title-format 
+      '("" (:eval (my-frame-title-format))))
+
+(show-paren-mode 1)
 
 
 ;; Setup python stuff
-(require 'virtualenv)
-
 (defun my-python-clear-buffer ()
   "Clear the python buffer, if it is running"
   (interactive)
@@ -104,8 +107,18 @@
 ;; Modify some keys in python mode
 (add-hook 'python-mode-hook
           '(lambda ()
-             (define-key python-mode-map "\C-c\C-v" 'virtualenv-run-python)
+             (setq  python-remove-cwd-from-path nil)
+             ;;(define-key python-mode-map "\C-c\C-v" 'virtualenv-run-python)
              (define-key python-mode-map "\C-c\C-k" 'my-python-clear-buffer)))
+
+(require 'virtualenv)
+
+
+;; better window movement
+(global-set-key (kbd "S-<left>")  'windmove-left)
+(global-set-key (kbd "S-<right>") 'windmove-right)
+(global-set-key (kbd "S-<up>")    'windmove-up)
+(global-set-key (kbd "S-<down>")  'windmove-down)
 
 
 ;; put the autosave and backup files in my emacs directory
@@ -130,36 +143,24 @@
 
 ;; yasnippet setup
 (require 'yasnippet)
-(yas/initialize)
+(yas-global-mode)
+(setq yas-prompt-functions '(yas-ido-prompt yas-no-prompt))
 
-;; Found at http://blog.iany.me/2012/03/use-popup-isearch-for-yasnippet-prompt/
-;; add some shotcuts in popup menu mode
-;; (define-key popup-menu-keymap (kbd "M-n") 'popup-next)
-;; (define-key popup-menu-keymap (kbd "M-p") 'popup-previous)
+;; Setup markdown mode
+(setq auto-mode-alist (cons '("\\.md\\'" . markdown-mode) auto-mode-alist))
 
-;; (defun yas/popup-isearch-prompt (prompt choices &optional display-fn)
-;;   (when (featurep 'popup)
-;;     (popup-menu*
-;;      (mapcar
-;;       (lambda (choice)
-;;         (popup-make-item
-;;          (or (and display-fn (funcall display-fn choice))
-;;              choice)
-;;          :value choice))
-;;       choices)
-;;      :prompt prompt
-;;      ;; start isearch mode immediately
-;;      :isearch t
-;;      )))
-
-(setq yas/prompt-functions '(yas/ido-prompt yas/no-prompt))
-
-;; Make C-c c do comment-region
-(define-key global-map (kbd "C-c c") 'comment-region)
-
+;; Setup expand-region
+(require 'expand-region)
+(global-set-key (kbd "C-,") 'er/expand-region)
 
 ;; Run Emacs as a server
 (require 'server)
 (unless (server-running-p)
   (server-start))
+
+;; Disable upcase-region warning
+(put 'upcase-region 'disabled nil)
+
+;; Enable Rust
+(setq auto-mode-alist (cons '("\\.rs\\'" . rust-mode) auto-mode-alist))
 
--- virtualenv.el
+++ virtualenv.el
@@ -1,30 +1,3 @@
-;;
-;; virtualenv.el - Use a virtualenv for python shell
-;;
-;; Copyright (c) 2012, Kipp Hickman
-;; All rights reserved.
-;;
-;; Redistribution and use in source and binary forms, with or without
-;; modification, are permitted provided that the following conditions are met:
-;;  * Redistributions of source code must retain the above copyright
-;;    notice, this list of conditions and the following disclaimer.
-;;  * Redistributions in binary form must reproduce the above copyright
-;;    notice, this list of conditions and the following disclaimer in the
-;;    documentation and/or other materials provided with the distribution.
-;;
-;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-;; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-;; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-;; A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-;; OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-;; SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-;; TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-;; PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-;; LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
 (require 'python)
 
 (defvar virtualenv-mode-name-format " [venv:%s]")
@@ -36,7 +9,6 @@
     (make-local-variable 'virtualenv-mode-name)
     (setq virtualenv-mode-name formatted-name)))
 
-;;;###autoload
 (define-minor-mode virtualenv-minor-mode
   "minor mode for showing virtualenv name in mode line"
   :init-value nil
@@ -47,22 +19,79 @@
   (and (file-exists-p env-path)
        (file-exists-p (concat (file-name-directory env-path)
                               "bin/activate_this.py"))))
-                
-  
-(defun virtualenv-run-python (activate)
+
+
+(defun virtualenv-activate (venv)
   (interactive "DVirtualenv Directory: ")
-  (unless (virtualenv-verify-p activate)
-    (error "Not a virtualenv directory: %s" activate))
-  (let* ((activate-this-py (expand-file-name
-                            (concat (file-name-directory activate)
-                                    "bin/activate_this.py")))
-         (execfile-cmd (format "execfile('%s', dict(__file__ = '%s'))"
-                               activate-this-py
-                               activate-this-py)))
-    (run-python)
-    (python-send-string execfile-cmd)
-    (virtualenv-set-mode-name
-     (car (last (split-string (directory-file-name activate) "/"))))
+  (let ((deactivate-choice))
+    (when virtualenv-minor-mode
+        (setq deactivate-choice
+              (y-or-n-p (format "Virtualenv %s already active. Deactivate it?"
+                                virtualenv-mode-name))))
+    (cond
+     ((and virtualenv-minor-mode (not deactivate-choice)) nil)
+     ((and virtualenv-minor-mode deactivate-choice)
+      (virtualenv-deactivate)
+      (virtualenv--activate-internal venv)
+      t)
+     (t (virtualenv--activate-internal venv) t))))
+
+(defun virtualenv--activate-internal (venv)
+  (unless (virtualenv-verify-p venv)
+    (error "Not a virtualenv directory: %s" venv))
+  (let* ((bin-path (expand-file-name (concat (file-name-directory venv) "bin")))
+         (old-path (getenv "PATH"))
+         (new-path (concat bin-path path-separator old-path)))
+    (virtualenv-set-mode-name 
+     (file-name-nondirectory (directory-file-name venv)))
+    (make-local-variable 'virtualenv--new-path)
+    (setq virtualenv--new-path new-path)
+    (make-local-variable 'virtualenv--venv)
+    (setq virtualenv--venv venv)
     (virtualenv-minor-mode t)))
+
+
+(defun virtualenv-deactivate ()
+  (interactive)
+  (if (not virtualenv-minor-mode)
+      (error "A virtualenv is in not active in this buffer")
+    (kill-local-variable 'virtualenv--new-path)
+    (kill-local-variable 'virtualenv--venv)
+    (virtualenv-minor-mode 0)))
+
+
+(defun virtualenv-run-python ()
+  (interactive)
+  (if (not virtualenv-minor-mode)
+      (if (call-interactively 'virtualenv-activate)
+          (let ((process-environment 
+               (cons (concat "VIRTUAL_ENV=" virtualenv--venv)
+                     (cons (concat "PATH=" virtualenv--new-path)
+                           process-environment))))
+            (run-python))
+        (message "Virtualenv not activated"))
+    (let ((process-environment 
+               (cons (concat "VIRTUAL_ENV=" virtualenv--venv)
+                     (cons (concat "PATH=" virtualenv--new-path)
+                           process-environment))))
+      (run-python))))
+
+
+;; (defun virtualenv-run-python (venv)
+;;   (interactive "DVirtualenv Directory: ")
+;;   (unless (virtualenv-verify-p venv)
+;;     (error "Not a virtualenv directory: %s" venv))
+;;   (let* ((activate-this-py (expand-file-name
+;;                             (concat (file-name-directory venv)
+;;                                     "bin/activate_this.py")))
+;;          (execfile-cmd (format "execfile('%s', dict(__file__ = '%s'))"
+;;                                activate-this-py
+;;                                activate-this-py)))
+;;     (run-python)
+;;     (python-send-string execfile-cmd)
+;;     (virtualenv-set-mode-name
+;;      (car (last (split-string (directory-file-name venv) "/"))))
+;;     (virtualenv-minor-mode t)))
+
 
 (provide 'virtualenv)
