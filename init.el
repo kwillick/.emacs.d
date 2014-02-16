7bdddc720e2b6216bed681ff8dd073b262d9f05a
Kipp Hickman <spud.kipp@gmail.com>
2014-02-16 00:48:52 -0800

    Lots of little changes.

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 (require 'cl-lib)
 
+(setq inhibit-splash-screen t)
+
 ;; Setup Emacs PATH
 (defun my-set-path-from-shell ()
   (let ((path-from-shell
@@ -12,13 +14,13 @@
 ;; Setup load-path
 (add-to-list 'load-path (expand-file-name user-emacs-directory))
 
-;; Add marmalade package archive
+;; Add marmalade and melpa package archives
 (require 'package)
 (defvar archive-marmalade '("marmalade" . "http://marmalade-repo.org/packages/"))
 (defvar archive-gnu '("gnu" . "http://elpa.gnu.org/packages/"))
 (defvar archive-melpa '("melpa" . "http://melpa.milkbox.net/packages/"))
 
-(add-to-list 'package-archives archive-marmalade)
+(add-to-list 'package-archives archive-marmalade t)
 (add-to-list 'package-archives archive-melpa t)
 (package-initialize)
 
@@ -37,12 +39,16 @@
 (defun my-install-packages-perform ()
   (my-install-packages
    (cons 'magit archive-marmalade)
-   (cons 'solarized-theme archive-marmalade)
-   (cons 'yasnippet archive-marmalade)
+   (cons 'solarized-theme archive-melpa)
+   (cons 'yasnippet archive-melpa)
    (cons 'markdown-mode archive-marmalade)
    (cons 'expand-region archive-marmalade)
-   (cons 'undo-tree archive-marmalade)
-   (cons 'd-mode archive-melpa)))
+   (cons 'rust-mode archive-melpa)
+   (cons 'd-mode archive-melpa)
+   (cons 'cmake-mode archive-marmalade)
+   (cons 'glsl-mode archive-melpa)
+   (cons 'rainbow-delimiters archive-melpa)
+   (cons 'smex archive-marmalade)))
 
 (condition-case nil
     (my-install-packages-perform)
@@ -55,27 +61,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(c-backslash-max-column 90)
  '(c-basic-offset 4)
- '(c-default-style 
-   (quote ((c-mode . "bsd") (c++-mode . "bsd") (java-mode . "bsd") 
-           (awk-mode . "awk") (other . "bsd"))))
+ '(c-default-style (quote ((c-mode . "bsd") (c++-mode . "bsd") (java-mode . "bsd") (awk-mode . "awk") (other . "bsd"))))
  '(c-echo-syntactic-information-p t)
  '(c-electric-pound-behavior (quote (alignleft)))
- '(c-hanging-braces-alist 
-   (quote ((defun-close) (class-close) (inline-close) (block-close) 
-           (statement-cont) (substatement-open after) (brace-list-open)
-           (brace-list-close) (brace-entry-open) (extern-lang-open after)
-           (namespace-open after) (namespace-close) (module-open after) 
-           (module-close) (composition-open after) (composition-close) 
-           (inexpr-class-open after) (inexpr-class-close before) 
-           (arglist-cont-nonempty))))
- '(c-offsets-alist 
-   (quote ((access-label . /) (arglist-close . 0) (inextern-lang . 0) (innamespace . 0))))
+ '(c-hanging-braces-alist (quote ((defun-close) (class-close) (inline-close) (block-close) (statement-cont) (substatement-open after) (brace-list-open) (brace-list-close) (brace-entry-open) (extern-lang-open after) (namespace-open after) (namespace-close) (module-open after) (module-close) (composition-open after) (composition-close) (inexpr-class-open after) (inexpr-class-close before) (arglist-cont-nonempty))))
+ '(c-offsets-alist (quote ((access-label . /) (arglist-close . 0) (inextern-lang . 0) (innamespace . 0))))
  '(c-tab-always-indent nil)
  '(c-toggle-auto-hungry-state nil)
  '(column-number-mode t)
  '(comint-process-echoes t)
- '(custom-safe-themes (quote ("501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" default)))
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(default-frame-alist (quote ((vertical-scroll-bars) (width . 100) (height . 66))))
  '(haskell-mode-hook (quote (turn-on-haskell-indent turn-on-haskell-doc-mode)))
  '(ido-ignore-files (quote ("\\`CVS/" "\\`#" "\\`.#" "\\`\\.\\./" "\\`\\./" "\\.DS_Store")))
@@ -89,6 +86,7 @@
  '(python-indent-guess-indent-offset nil)
  '(scroll-bar-mode nil)
  '(standard-indent 2)
+ '(js-indent-level 2)
  '(tab-width 4)
  '(tool-bar-mode nil))
 
@@ -97,18 +95,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "apple" :family "Monaco")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "apple" :family "Monaco")))))
+
+
+;; Load theme
+(require 'solarized)
+(require 'solarized-theme)
+(load-theme 'solarized-dark t)
+
 
 ;; This macro is a helper around eval-after-load + progn
 (defmacro after-load (mode &rest body)
   (declare (indent defun))
   `(eval-after-load ,mode
      '(progn ,@body)))
-
-
-;; Load theme
-(require 'solarized-theme)
-(load-theme 'solarized-dark t)
 
 
 ;; Format title to show file path or buffer name
@@ -126,11 +126,11 @@
 
 (defvar clean-mode-line-clean-alist
   '((yas-minor-mode . " y")
-    (undo-tree-mode . " U")
     (abbrev-mode . "")
     ;; major modes
     (python-mode . "py")
-    (emacs-lisp-mode "el")))
+    (emacs-lisp-mode . "el")
+    (js-mode . "js")))
 
 (defun clean-mode-line ()
   (interactive)
@@ -146,19 +146,30 @@
 (add-hook 'after-change-major-mode-hook 'clean-mode-line)
 
 ;; shell stuff
-(after-load 'pcomplete
-  (setq pcomplete-ignore-case t))
+(defun get-first-shell ()
+  "Get first shell listed in /etc/shells"
+  (with-temp-buffer
+    (insert-file-contents "/etc/shells")
+    (forward-line 4)
+    (let ((p (point)))
+      (end-of-line)
+      (buffer-substring p (point)))))
 
 (after-load 'shell
-  (defun get-user-shell ()
-    (with-temp-buffer
-      (insert-file-contents "/etc/shells")
-      (forward-line 4)
-      (let ((p (point)))
-        (end-of-line)
-        (buffer-substring p (point)))))
-  (setq explicit-shell-file-name (get-user-shell))
+  ;; (defadvice shell (before shell-check-remote (&optional buffer) activate)
+  ;;   (setq explicit-shell-file-name (if (file-remote-p default-directory)
+  ;;                                      nil
+  ;;                                    (get-first-shell))))
+
   (add-to-list 'explicit-bash-args "--login"))
+
+(require 'pcomplete)
+(defun my-shell-mode-hook ()
+  "set pcomplete-ignore-case to t. shell-dynamic-complete-functions uses pcomplete."
+  (make-local-variable 'pcomplete-ignore-case)
+  (setq pcomplete-ignore-case t))
+
+(add-hook 'shell-mode-hook 'my-shell-mode-hook)
 
 ;; Open init file
 (defun goto-init-file ()
@@ -168,12 +179,19 @@
 
 
 ;; Setup ido-mode
+(require 'ido)
 (ido-mode 1)
 (setq ido-enable-flex-matching t
       ido-everywhere t
       ido-create-new-buffer 'always
       confirm-nonexistent-file-or-buffer nil)
+(global-set-key (kbd "C-x C-d") 'ido-dired) ;; Switch list directory to dired
 
+;; Setup smex
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 (defun find-file-as-root ()
   "Call ido-find-file using tramp/sudo"
@@ -184,16 +202,13 @@
     (find-file file)))
 
 
-;; undo-tree
-(global-undo-tree-mode)
-
 ;; Setup python stuff
 (after-load 'python
   (defun python-set-virtualenv-path (dir)
     "Set the python-shell-virtualenv-path to dir. If already set, set it to nil."
     (interactive (if (null python-shell-virtualenv-path)
                      (list (expand-file-name 
-                            (read-directory-name "virtualenv dir: " nil nil t)))
+                            (ido-read-directory-name "virtualenv dir: " nil nil t)))
                    (list nil)))
     (setq python-shell-virtualenv-path dir))
 
@@ -228,6 +243,24 @@
         ad-do-it)))
 )
 
+;; javscript stuff
+;; which-function-mode does not work with js very well
+(add-hook 'js-mode-hook
+          (lambda ()
+            (which-function-mode -1)
+            (rainbow-delimiters-mode)))
+
+;; emacs-lisp stuff
+(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+
+;; magit stuff
+(defun my-magit-stage-region (start end)
+  (interactive "r")
+  (let ((count (count-lines start end)))
+    (goto-char start)
+    (dotimes (var count)
+      (magit-stage-item))))
+
 
 ;; Nicer window movement
 (global-set-key (kbd "S-<left>")  'windmove-left)
@@ -254,9 +287,6 @@
 (yas-global-mode)
 (setq yas-prompt-functions '(yas-ido-prompt yas-no-prompt))
 
-;; Setup markdown mode
-(setq auto-mode-alist (cons '("\\.md\\'" . markdown-mode) auto-mode-alist))
-
 ;; Setup expand-region
 (require 'expand-region)
 (global-set-key (kbd "C-,") 'er/expand-region)
@@ -269,11 +299,6 @@
 ;; Disable upcase-region warning
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
-
-;; Enable addition programming language modes
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
-(add-to-list 'auto-mode-alist '("\\.d[i]?\\'" . d-mode))
-
 
 ;; Better indent function
 (defun indent-region-or-buffer ()
@@ -305,7 +330,6 @@
 (global-set-key (kbd "M-S-<up>") 'my-move-line-up)
 (global-set-key (kbd "M-S-<down>") 'my-move-line-down)
 
-
 ;; indirect buffer + narrowing
 (defun my-narrow-to-region-indirect (start end)
   "Restrict editing in this buffer to the current region, indirectly."
@@ -319,3 +343,23 @@
 (defun byte-compile-this-file ()
   (interactive)
   (byte-compile-file (buffer-file-name)))
+
+
+;; cmake-mode
+(require 'cmake-mode)
+
+
+;; Modify auto-mode-alist
+(add-to-list 'auto-mode-alist '("\\.mm\\'" . objc-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+(add-to-list 'auto-mode-alist '("\\.d[i]?\\'" . d-mode))
+(add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-mode))
+(add-to-list 'auto-mode-alist '("\\.cmake\\'" . cmake-mode))
+(add-to-list 'auto-mode-alist '("\\.vsh\\'" . glsl-mode))
+(add-to-list 'auto-mode-alist '("\\.fsh\\'" . glsl-mode))
+(add-to-list 'auto-mode-alist '("\\.gsh\\'" . glsl-mode))
+
+
+;; Change to ~/
+(cd "~/")
